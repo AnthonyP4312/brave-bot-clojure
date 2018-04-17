1c65005e787076d47ad87043eb25fe68fe41634c
Anthony Palmer <AnthonyP4312@gmail.com>
2018-04-17 03:33:15 +0000

    Added method to retrieve websocket urls

--- project.clj
+++ project.clj
@@ -1,6 +1,12 @@
 (defproject brave-bot "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
+  :main brave-bot.core
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-http "3.8.0"]
+                 [stylefruits/gniazdo "1.0.1"]
+                 [cheshire "5.8.0"]]
+  :repositories {"local" ~(str (.toURI (java.io.File. "maven_repository")))})
+
--- src/brave_bot/core.clj
+++ src/brave_bot/core.clj
@@ -1,6 +1,29 @@
-(ns brave-bot.core)
+(ns brave-bot.core
+  (:gen-class)
+  (:require [clj-http.client :as http]
+            [gniazdo.core :as ws]
+            [cheshire.core :as JSON :refer [parse-string]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn url
+  [& routes]
+  (apply str (interpose "/" (cons "https://discordapp.com/api" (flatten routes)))))
+
+(url "boiii")
+
+(def ws-atom (atom ""))
+(defn ws-url
+  "Return a new wss url from the discord api"
+  ([] (ws-url false))
+  ([reset]
+   (cond
+     (= @ws-atom "") (reset! ws-atom (:url (parse-string (:body (http/get (url "gateway"))) true)))
+     (true? reset)   (reset! ws-atom (:url (parse-string (:body (http/get (url "gateway"))) true)))
+     :else           @ws-atom)))
+
+(ws-url)
+
+(defn -main
+  "Initializes the bot and caches websocket url"
+  []
+  (println "Hello, World!"))
+
